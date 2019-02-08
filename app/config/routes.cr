90bf6b454cddd7ca323534139e05172aa3f9281d
Damian Hamill <damianham@gmail.com>
2019-02-08 13:00:16 +0700

    add app, controller, model, scaffold generators

---
+++ app/config/routes.cr
@@ -0,0 +1,52 @@
+Amber::Server.configure do |app|
+  pipeline :web do
+    # Plug is the method to use connect a pipe (middleware)
+    # A plug accepts an instance of HTTP::Handler
+    plug Amber::Pipe::PoweredByAmber.new
+    # plug Amber::Pipe::ClientIp.new(["X-Forwarded-For"])
+    plug Citrine::I18n::Handler.new
+    plug Amber::Pipe::Error.new
+    plug Amber::Pipe::Logger.new
+    plug Amber::Pipe::Session.new
+    plug Amber::Pipe::Flash.new
+    plug Amber::Pipe::CSRF.new
+
+    # enable this pipe for JWT support however you need a User model
+    # 'amber g auth' will create a User model and signin/signup routes
+    # it will also add 'plug Authenticate.new' which you should disable
+    # plug AuthenticateJWT.new
+  end
+
+  pipeline :api do
+    plug Amber::Pipe::PoweredByAmber.new
+    plug Amber::Pipe::Error.new
+    plug Amber::Pipe::Logger.new
+    plug Amber::Pipe::Session.new
+    plug Amber::Pipe::CORS.new
+  end
+
+  # All static content will run these transformations
+  pipeline :static do
+    plug Amber::Pipe::PoweredByAmber.new
+    plug Amber::Pipe::Error.new
+    plug Amber::Pipe::Static.new("./public")
+  end
+
+  routes :web do
+    get "/authenticateWithToken", HomeController, :authenticate_jwt
+    get "/me", HomeController, :me
+    websocket "/model", ModelSocket
+    websocket "/signal", SignalSocket
+    get "/", HomeController, :index
+    post "/", HomeController, :token_sign_in_helper
+  end
+
+  routes :api do
+  end
+
+  routes :static do
+    # Each route is defined as follow
+    # verb resource : String, controller : Symbol, action : Symbol
+    get "/*", Amber::Controller::Static, :index
+  end
+end
